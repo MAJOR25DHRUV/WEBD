c556b411e073854af2c26ebf67af2a73cbdb252b
Dhruv kushwah <dhruvkushwah243@gmail.com>
2023-12-13 03:24:54 +0000

    blog

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,14 @@
-function App() {
+import React, { useState } from "react";
+
+export default function App() {
+  // State variables: fn and sn
+  const [fn, setFn] = useState(14);
+  const [sn, setSn] = useState(15);
+
   return (
-    <div className="App">
-      apppppppp
+    <div className="outerdiv">
+      {/* Display the sum of fn and sn */}
+      <p>{fn + sn}</p>
     </div>
   );
 }
-
-export default App;
