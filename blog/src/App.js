e054bfdcaab473f5476fcca511a2a04f96a2df12
Dhruv kushwah <dhruvkushwah243@gmail.com>
2023-12-14 13:08:58 +0000

    To Do And Calculator

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,47 @@
-import React, { useState } from "react";
+// src/App.js
+import React, { useState } from 'react';
+import TodoForm from './components/ToDoForm';
+import TodoList from './components/ToDoList';
+import Calculator from './components/Calculator';
+import Header from './components/Header';
+import './App.css'; // You can create a global CSS file for your app-wide styles
 
-export default function App() {
-  // State variables: fn and sn
-  const [fn, setFn] = useState(14);
-  const [sn, setSn] = useState(15);
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (todo) => {
+    setTodos((prevTodos) => [...prevTodos, todo]);
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleUpdateTodo = (id, newText) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, text: newText } : todo
+      )
+    );
+  };
 
   return (
-    <div className="outerdiv">
-      {/* Display the sum of fn and sn */}
-      <p>{fn + sn}</p>
+    <div className="app">
+      <div className="todo-section">
+        <h1>React Todo List</h1>
+        <TodoForm onAdd={handleAddTodo} />
+        <TodoList
+          todos={todos}
+          onDelete={handleDeleteTodo}
+          onUpdate={handleUpdateTodo}
+        />
+      </div>
+      <div className="calculator-section">
+        <h1>React Calculator</h1>
+        <Calculator />
+      </div>
     </div>
   );
 }
+
+export default App;
